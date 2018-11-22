cc8823df2297427ab73e4f39392fa11134bc678d
jmahon <joseph.mahon@nhs.net>
2018-11-22 10:52:30 +0000

    Added lrg object creation function and lrg parsing functions

--- lrgparser.py
+++ lrgparser.py
@@ -6,23 +6,38 @@ import argparse
 import os
 import sys
 
+# XML Related Imports
+import xml.etree.ElementTree as ET
 
 
-def lrgparser():
-	'''Main code for argparser here'''
-	pass
+class lrgobject:
+	'''LRG object class containing LRG ID, HGNC ID etc'''
+	def __init__(self, lrg_id, hgnc_id, seq_source, mol_type):
+		self.lrg_id = lrg_id
+		self.hgnc_id = hgnc_id
+		self.seq_source = seq_source
+		self.mol_type = mol_type
+		self.
 
 
-def get_exon_locs():
-	pass
+def main(xml_file):
+	'''Main function'''
+	lrg_object = lrg_object_creator(xml_file)
+	bed_file = bedgen.generate_bed(lrg_object)
 
-def get_intron_locs()
-	pass
 
-def generate_bed_file():
-	pass
-
+def lrg_object_creator(xml_file):
+	'''Returns an LRG object when passed an LRG XML file. Object contains 
+	lrg_id, hgnc_id, seq_source, mol_type, a dict of exons and locations.
+	'''
+	tree = ET.parse(xml_file)
+	root = tree.getroot()
+	lrg_id = root.find('fixed_annotation/id').text
+	hgnc_id = root.find('fixed_annotation/hgnc_id').text
+	seq_source = root.find('fixed_annotation/sequence_source').text
+	mol_type = root.find('fixed_annotation/mol_type').text
+	#exons = functions.get_exon_locs(root)
 
 
 in __name__ == "__main__":
-	lrgparser()
+	main('LRG_384.xml')
