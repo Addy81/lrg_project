d1f1d3945e9a57fbe4cea88b8a20905001c31d46
Adriana Toutoudaki <adriana.tou@gmail.com>
2018-12-06 10:17:29 +0000

    removed redundant code

--- at_practice.py
+++ at_practice.py
@@ -211,3 +211,43 @@ def get_flanked_coords(exon_coords,flank=5):
 print('---------------------------------------------------------')
         
 print (get_flanked_coords(get_chr_coordinates(root ,'GRCh38.p12','NM_000257.2'), 10))
+
+"""
+
+
+
+def get_exon_coords(root, lrg_id):
+    ''' Pull LRG exon coordinates'''
+    exon_coords = {}
+
+    for exon in root.iter('exon'):
+        if len(exon.attrib) == 1:
+            for coord in exon.iter('coordinates'):
+                coord_list = []
+                if coord.attrib['coord_system'] == lrg_id:
+                    coord_list.append(int(coord.attrib['start']))
+                    coord_list.append(int(coord.attrib['end']))
+                    exon_coords[exon.attrib['label']] = coord_list
+                else:
+                    pass
+    return exon_coords
+         
+
+def get_real_exon_coords(root, NM_number):
+    ''' Extract the exon coordinates that correspond to a specific transcript'''
+
+    nm_exon_coordinates = {}
+    for mapping in root.iter('mapping'):
+        if NM_number == mapping.attrib['coord_system']:
+            count = 0
+            for item in mapping.iter('mapping_span'): 
+                count +=1
+                exon_coord_list = []
+                exon_coord_list.append(int(item.attrib['other_start']))
+                exon_coord_list.append(int(item.attrib['other_end']))
+                nm_exon_coordinates[count] = exon_coord_list
+        elif NM_number != mapping.attrib['coord_system']:
+            pass
+    return nm_exon_coordinates
+
+    """
