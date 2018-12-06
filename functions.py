1f97ef2c6f0ffa8eef7eae5c6e03856e6c92a00c
jmahon <joseph.mahon@nhs.net>
2018-12-06 16:44:00 +0000

    Cleanup and reformat to meet PEP8

--- functions.py
+++ functions.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-#
-#
-#
-#
-# Adriana Toutoudaki (October 2018), contact: adriana.tou@gmail.com
+# -*- coding: utf-8 -*-
 
-#script to parse xml and pick out some elements
+"""Module containing exon/intron extraction and calculation functions."""
+
+
 import xml.etree.ElementTree as ET 
 
 
 def get_exon_coords(root, genome_choice, transcript_choice):
-    '''Calculates genomic coordinates based on the mapping information for different reference genomes and transcripts. '''
-    
+    """Calculates the genomic coordinates of each exon. It uses the mapping
+    information in the XML and the genome build and transcript provided by 
+    the user.
+    """
     mapped_coordinates = {}
     
-    #Iterates from the root of the xml tree to identify the mapping attributes
+    # Iterates from the root of the xml tree to identify mapping attributes.
     for mapping in root.iter('mapping'):
-        #Checks the reference genome requested by the user and extracts the correct mapping start and end coordinates
+        # Checks the reference genome requested by the user and extracts the
+        # correct mapping start and end coordinates.
         if genome_choice == mapping.attrib['coord_system']:
             for mapping_span in mapping.iter('mapping_span'):
                 mapped_start = int(mapping_span.attrib['other_start']) - 1
                 mapped_end = int(mapping_span.attrib['other_end']) + 1
                 strand = mapping_span.attrib['strand']
 
-        #Checks the transcript requested by the user and calculates the genomic coordinates accordingly    
+        # Checks the transcript requested by the user and calculates the 
+        # genomic coordinates accordingly.
         if transcript_choice == mapping.attrib['coord_system']:
             count = 0
             
@@ -31,7 +33,8 @@ def get_exon_coords(root, genome_choice, transcript_choice):
                 count += 1
                 coordinates = []
 
-                # Option of whether the gene is on the forward or reverse strand
+                # Mapping differs depending on whether the gene is on the 
+                # forward or reverse strand
                 if strand == '1':
                     exon_start = mapped_start + int(exon.attrib['lrg_start'])
                     exon_end = mapped_start + int(exon.attrib['lrg_end'])
@@ -42,28 +45,32 @@ def get_exon_coords(root, genome_choice, transcript_choice):
                     exon_end = mapped_end - int(exon.attrib['lrg_end'])
                     coordinates.append(exon_start)
                     coordinates.append(exon_end)
-                # Coordinates are stored in a dictionary with exon numbers as keys and start stop coordinates as values i.e. {1: [23904870, 23904829]}
+                # Coordinates are stored in a dictionary with exon numbers as 
+                # keys, with start and stop coordinates as values 
+                # i.e. {'1': [23904870, 23904829]}
                 mapped_coordinates[count] = coordinates
             
     return mapped_coordinates
 
 
 def get_intron_coords(exon_coords):
-    """ Calculate intron coordinates"""
-
+    """Calculates the genomic coordinates of each intron. It uses the already
+    mapped exon coordinates, and maps an intron as the gap between each exon.
+    Introns coordinates do not include flanking regions.
+    """
     intron_coords = {}
 
     for key in exon_coords.keys():
-        # Stopping at the last exon as if exon number is n, intron number will be n-1
+        # Stop at the last exon as total exons = n, total introns = n-1
         if (key+1) not in exon_coords.keys():
             break
         else:
-            # checks if gene is in foward strand
+            # Check if the gene is on the foward strand
             if (exon_coords[1][1] - exon_coords[1][0]) > 0:
                 intron_start = exon_coords[key][1] + 1 
                 intron_end = exon_coords[key+1][0] - 1
                 intron_coords[key] = [intron_start, intron_end]
-            # checks if gene is in reverse strand
+            # Check if the gene is on the reverse strand
             elif (exon_coords[1][1] - exon_coords[1][0]) < 0:
                 intron_start = exon_coords[key][1] - 1 
                 intron_end = exon_coords[key+1][0] + 1
@@ -72,8 +79,8 @@ def get_intron_coords(exon_coords):
     return intron_coords
 
 
-def get_flanked_coords(exon_coords,flank=5):
-    """ Provides adjuedted exon coordinates based on user input"""
+def get_flanked_coords(exon_coords,flank=0):
+    """Adjusts the flanking regions for each exon based on user input."""
 
     flanked_coords = {}
 
