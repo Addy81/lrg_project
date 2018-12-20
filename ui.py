c4be5244544a10e9bbd21fceb40a3e97fc0e7ef1
jmahon <joseph.mahon@nhs.net>
2018-12-20 15:10:14 +0000

    Added check to see whether unittest is being performed. If testing is performed, don't use os.system(clear)

--- ui.py
+++ ui.py
@@ -14,8 +14,9 @@ def determine_if_show_ui(args):
 		show_ui = False
 	return show_ui
 
-def splashscreen():
-	os.system("clear")
+def splashscreen(testing=False):
+	if testing == False:
+		os.system("clear")
 	print("="*40)
 	print("")
 	print("    LRG File Parsing Program")
