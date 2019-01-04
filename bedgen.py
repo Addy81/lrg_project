35dbead5a275b130bc1500e83dd2d24e5297d181
jmahon <joseph.mahon@nhs.net>
2019-01-04 14:15:02 +0000

    Added detailed docstrings and SystemExit exception if can't write to file

--- bedgen.py
+++ bedgen.py
@@ -3,8 +3,20 @@
 import csv
 
 def create_bed_contents(lrg_object, introns_choice):
+	"""Creates the contents for the BED file. Returns a nested list with
+	each row being [chromosome, start, end, label]
+	
+	Args:
+		lrg_object (dict): An LRG_Object containing LRG ID, HGNC name etc
+		introns_choice (bool): Include introns True or False
+	Returns:
+		bedcontents (list): Nested list of rows [chromosome, start, end, label]
+
+	"""
+
 	bedcontents = []
-	# Add exon rows
+	
+	# Adds exon rows
 	for item in lrg_object.mapped_flanked_exon_coords:
 		chromosome = "chr"+lrg_object.chromosome
 		start = lrg_object.mapped_flanked_exon_coords[item][0]
@@ -15,7 +27,8 @@ def create_bed_contents(lrg_object, introns_choice):
 		else:
 			pass
 		bedcontents.append([chromosome, start, end, label])
-		# Add intron rows
+	
+	# Adds intron rows
 	if introns_choice == True:
 		for item in lrg_object.mapped_intron_coords:
 			chromosome = "chr"+lrg_object.chromosome
@@ -27,12 +40,24 @@ def create_bed_contents(lrg_object, introns_choice):
 				pass
 			label = "Intron_"+str(item)
 			bedcontents.append([chromosome, start, end, label])
-	else:
-		pass
+
 	return bedcontents
 
 
 def write_bed_file(filetowrite, bedheader, bedcontents):
+	"""Writes the BED header and contents to file.
+
+	Args:
+		filetowrite (str): The filename to write to
+		bedheader (list): The header to write. List of strings
+		bedcontents (list): Nested list of rows [chromosome, start, end, label]
+	Returns:
+		Nothing
+	Raises:
+		SystemExit: If the file could not be written
+
+	"""
+
 	try:
 		with open(filetowrite, 'w', newline='') as csv_file:
 			writer = csv.writer(csv_file, delimiter="\t")
@@ -44,3 +69,4 @@ def write_bed_file(filetowrite, bedheader, bedcontents):
 		print("")
 	except:
 		print("    Could not write BED file. Check write permissions")
+		raise SystemExit
